997c9f120e6d8289b615bb0972a74e22d569361e
Andreas Tor Mortensen <andr997c@gmail.com>
2022-10-06 01:36:04 +0200

    Graph class implemented

--- heartrunner/pathfinder.py
+++ heartrunner/pathfinder.py
@@ -1,17 +1,44 @@
-from typing import List
+from typing import Dict, List, Tuple
 from .types import *
 
 class Graph:
-    def __init__(
-        self, 
-        streetsegments: List[StreetSegment],
-        intersections: List[Intersection],
-        patients: List[Patient],
-        runners: List[Runner],
-        aeds: List[Aed]
-    ) -> None:
-        pass
+    nodes: Dict[int, Intersection]
+    edges: Dict[int, StreetSegment]
+    adj_list: Dict[int, List[Tuple]]
+    runners: Dict[int, List[Runner]]
+    aeds: Dict[int, List[Aed]]
+    
+    def __init__(self):
+        self.nodes = {}
+        self.edges = {}
+        self.adj_list = {}
+        self.runners = {}
+        self.aeds = {}
+
+    def add_node(self, node: Intersection):
+        if node.id not in self.nodes:
+            self.nodes[node.id] = node
+            self.adj_list[node.id] = []
+
+    def add_edge(self, edge: StreetSegment):
+        self.edges[edge.id] = edge
+
+        head_id = edge.head_intersection_id
+        tail_id = edge.tail_intersection_id
+        
+        self.adj_list[head_id].append( (edge.id, tail_id) )
+        self.adj_list[tail_id].append( (edge.id, head_id) )
+
+    def add_aed(self, aed: Aed, intersection_id: int):
+        if intersection_id not in self.aeds:
+            self.aeds[intersection_id] = []
+        self.aeds[intersection_id].append(aed)
+    
+    def add_runner(self, runner: Runner, intersection_id: int):
+        if intersection_id not in self.runners:
+            self.runners[intersection_id] = []
+        self.runners[intersection_id].append(runner)
 
 class Pathfinder:
-    def __init__(self, graph: Graph) -> None:
-        pass
+    def __init__(self, graph: Graph):
+        self.graph = graph
--- heartrunner/types.py
+++ heartrunner/types.py
@@ -2,32 +2,31 @@ import itertools
 from random import randrange
 
 class Aed:
-    def __init__(self, id, intersection_id, time_range):
+    def __init__(self, id, intersection_id, time_range, in_use = 'false'):
         self.id = id
         self.start_hour, self.close_hour = time_range
         self.intersection_id = intersection_id
-        self.csv = [self.id, self.intersection_id, 'false', self.start_hour, self.close_hour]
+        self.in_use = in_use
+        self.csv = [self.id, self.intersection_id, self.in_use, self.start_hour, self.close_hour]
 
 class Runner:
     id_iter = itertools.count(1)
 
-    def __init__(self, id = None, speed = None, intersection_id = None):
+    def __init__(self, id = None, speed = None):
         self.id = next(self.id_iter) if id == None else id
         self.speed = randrange(3,6) if speed == None else speed
-        self.intersection_id = intersection_id
 
     def create_node(self):
         return f"CREATE (p:Runner {{id: {self.id}, speed: {self.speed}}})"
-
+    
     def row2runner(row):
         return {"id": row["p"]["id"], "speed": row["p"]["speed"]}
 
 class Patient:
     id_iter = itertools.count(1)
 
-    def __init__(self, id = None, intersection_id = None):
+    def __init__(self, id = None):
         self.id = next(self.id_iter) if id == None else id
-        self.intersection_id = intersection_id
 
     def create_node(self):
         return f"CREATE (p:Patient {{id: {self.id}}})"
