176ebb7ff3efaf12ec94ac06d91fd5e999053a03
Sullfred <74530142+Sullfred@users.noreply.github.com>
2022-10-23 21:24:14 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,24 @@
 import unittest
 
-class TestRewards(unittest.TestCase):
-    def test_gain():
+# Import functions from files here:
+import heartrunner.HRtypes
+
+
+# Basic Test class 
+class TestTypes(unittest.TestCase):
+
+    runner = heartrunner.HRtypes.Runner
+    patient = heartrunner.HRtypes.Patient
+
+    # Test case function
+    def test_Runner(self):
+        runnerId = self.runner.id_iter
+        self.assertIsNotNone(runnerId)
+
+    def test_patient(self):
+        patientId = self.patient.id_iter
+        self.assertIsNotNone(patientId)
+
+if __name__ == '__main__':  
+    # begin the unittest.main()  
+    unittest.main()  
