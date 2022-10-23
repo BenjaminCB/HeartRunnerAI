bb8633e225eed7fb618573ff38104ba34038df55
BenjaminCB <benjamincb@hotmail.dk>
2022-10-23 17:44:08 +0200

    update process_tasks method

--- heartrunner/environment.py
+++ heartrunner/environment.py
@@ -30,12 +30,30 @@ class Environment:
             timestep = task[1]
             self.state -= timestep
 
-            # calculate patient_times, figure out which value to give unreachable runners
-            # calculate aed_times
-            # concat all into big vector
-            # scale values from 0 to 1 look at np.interp
-            # get prediction from nn
+            # save relative position so we can retrieve the id later
+            runner_ids = []
+            truncated_state = np.empty(len(runner_ids))
+            state_through_patient = np.empty(len(runner_ids))
+            state_through_aed = np.empty(len(runner_ids))
+            i = 0
+            for runner, ptime, atime in runner_times:
+                runner_ids.append(runner.id)
+                truncated_state[i] = self.state[runner_ids[i]]
+                state_through_patient[i] = truncated_state[i] + ptime
+                state_through_aed[i] = truncated_state[i] + atime
+                i += 1
+
+            # concat and normalize data
+            nn_input = np.concatenate((truncated_state, state_through_patient, state_through_aed), axis=None)
+            nn_input = (nn_input - np.min(nn_input)) / (np.max(nn_input) - np.min(nn_input))
+
+            prediction = self.nn.predict(nn_input)
+
             # update state
+            self.state[runner_ids[prediction[0]]] += runner_times[prediction[0]][1]
+            self.state[runner_ids[prediction[1]]] += runner_times[prediction[1]][2]
+
             # update score from new state
+            self.score += self.state.max
 
         self.tasks = []
