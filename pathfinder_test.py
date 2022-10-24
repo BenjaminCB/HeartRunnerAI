a0cac92230849832346d19425cc3076854153880
Andreas Tor Mortensen <andr997c@gmail.com>
2022-10-24 19:09:36 +0200

    Implemented a default singleton database for easier access

--- pathfinder_test.py
+++ pathfinder_test.py
@@ -1,25 +1,7 @@
-import os
-from dotenv import load_dotenv
-from timeit import default_timer
 from heartrunner.types import NodeType
 from heartrunner.database import HeartrunnerDB
 
 if __name__ == "__main__":
-    load_dotenv('.env')
-    uri = os.getenv("NEO4J_URI")
-    user = os.getenv("NEO4J_USERNAME")
-    password = os.getenv("NEO4J_PASSWORD")
-    with HeartrunnerDB(uri, user, password) as db:
-        acc = 0
-        for i in range(10):
-            db.delete_nodes(NodeType.Runner)
-            db.generate_runners(1000)
-            time1 = default_timer()
-            db.delete_nodes(NodeType.Patient)
-            for patient in db.generate_patients(1):
-                tasks = db.get_pathfinder(patient).calculate_tasks(n_runners=20, n_aeds=3)
-                
-            time2 = default_timer()
-            elapsed = time2-time1
-            acc += elapsed
-            print(f"{i+1:4} Elapsed: {elapsed:.6f} - Average: {acc/(i+1):.6f}")
+    with HeartrunnerDB.default() as db:
+        runners = db.count_nodes(NodeType.Runner)
+        print(runners)
