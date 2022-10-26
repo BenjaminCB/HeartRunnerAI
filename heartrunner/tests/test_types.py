1a7d8d67a616f1f0c0005fa7aab119a6c0037e66
Andreas Tor Mortensen <andr997c@gmail.com>
2022-10-26 17:44:57 +0200

    added sample tests

--- heartrunner/tests/test_types.py
+++ heartrunner/tests/test_types.py
@@ -1,8 +1,28 @@
-from heartrunner.types import Runner
+from heartrunner.types import Patient, Runner
+
 
 def test_runner_ids():
-    for i in range(100):
+    for i in range(1000):
         r = Runner()
-    r = Runner()
-    assert r.id == 100 
+        assert r.id == i
+    
 
+
+def test_runner_speeds():
+    for i in range(1000):
+        r = Runner()
+        assert r.speed > 3 and r.speed < 6
+
+
+# TODO: test_runner_from_neo4j
+
+
+def test_patient_ids():
+    for i in range(1000):
+        p = Patient()
+        assert p.id == i
+
+    assert Patient(id=1).id == 1
+
+
+# TODO: test_patient_from_neo4j
