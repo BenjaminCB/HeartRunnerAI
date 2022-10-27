640d6df9e2768e57f89088a7e2eae7095f43ee38
Andreas Tor Mortensen <andr997c@gmail.com>
2022-10-27 18:37:46 +0200

    Messing with pytest

---
+++ heartrunner/tests/test_patient.py
@@ -0,0 +1,26 @@
+import pytest
+from heartrunner.types import Patient, Intersection
+
+def test_ids():
+    for i in range(1000):
+        assert Patient().id == i
+    
+    Patient.reset_id()
+    assert Patient().id == 0
+
+    assert Patient(id=10).id == 10
+
+
+def test_to_neo4j():
+    with pytest.raises(AttributeError):
+        Patient().to_neo4j()
+    id = 1
+    inter = Intersection(id=id, coords=(0,0))
+    expected = {
+        "id": id,
+        "intersection_id": inter.id
+    }
+    assert Patient(id=id, intersection=inter).to_neo4j() == expected
+
+
+# TODO: test_from_neo4j - lookinto fixtures for db mocking
---
+++ heartrunner/tests/test_runner.py
@@ -0,0 +1,36 @@
+import pytest
+from heartrunner.types import Intersection, Runner
+
+
+def test_ids():
+    for i in range(1000):
+        assert Runner().id == i
+
+    Runner.reset_id()
+    assert Runner().id == 0
+
+    assert Runner(id=10).id == 10
+
+
+def test_speeds():
+    for i in range(1000):
+        speed = Runner().speed
+        assert speed < 6 and speed > 3
+
+
+def test_to_neo4j():
+    with pytest.raises(AttributeError):
+        Runner().to_neo4j()
+    id = 1
+    speed = 5
+    inter = Intersection(id=id, coords=(0, 0))
+    expected = {
+        "id": id,
+        "speed": speed,
+        "intersection_id": inter.id
+    }
+    assert Runner(id=id, speed=speed,
+                  intersection=inter).to_neo4j() == expected
+
+
+# TODO: test_from_neo4j - lookinto fixtures for db mocking
