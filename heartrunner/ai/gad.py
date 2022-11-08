d03bbeb52e175f3fb9fbef6e065f3048f6ed9d64
BenjaminCB <benjamincb@hotmail.dk>
2022-11-08 12:58:20 +0100

    cleanup

--- heartrunner/ai/gad.py
+++ heartrunner/ai/gad.py
@@ -15,21 +15,24 @@ PREV_TIME = 0
 SAMPLE_CHANCE = 0.03
 
 
+# return a sorted sample from the list of tasks
 def sample_n_tasks(n: int):
     sampled = sample(ALL_TASKS, n)
     return sorted(sampled, key=lambda t: t.time)
 
 
+# return a sorted sample from the list of tasks
 def sample_tasks(pick_chance: float):
     sampled = []
 
     for task in ALL_TASKS:
-        if uniform(0,1) < pick_chance:
+        if uniform(0, 1) < pick_chance:
             sampled.append(task)
 
     return sampled
 
 
+# adjust state latencies as time passes
 def update_state_time(t: hct.Task):
     global PREV_TIME, STATE
 
@@ -43,7 +46,6 @@ def greedy(task_count: int):
     global STATE, PREV_TIME
 
     tasks = sample_n_tasks(task_count)
-    count = 1
     for task in tasks:
         update_state_time(task)
         min_cost = task.p_costs[0] + STATE[task.runners[0]] + task.a_costs[1] + STATE[task.runners[1]]
@@ -72,27 +74,36 @@ def greedy(task_count: int):
     PREV_TIME = 0
 
 
+# make a prediction for a single choice such a which runner for the patient
 def predict_with_cost(runners: list[int], cost: list[int], sol_idx: int):
     global GANN_instance, STATE
 
     # make prediction for patient runner and update state
     truncated_state = numpy.take(STATE, runners)
-    concated = numpy.concatenate((truncated_state, numpy.array(cost)), axis=None)
-    input = numpy.array([concated / numpy.amax(concated)])
-    # input = numpy.array([concated])
+    concat = numpy.concatenate((truncated_state, numpy.array(cost)), axis=None)
+    nn_input = numpy.array([concat / numpy.amax(concat)])
+    # input = numpy.array([concatenated])
     predictions = pygad.nn.predict(last_layer=GANN_instance.population_networks[sol_idx],
-                                   data_inputs=input)
+                                   data_inputs=nn_input)
     c_idx = predictions[0]
     STATE[runners[c_idx]] += cost[c_idx]
 
     return c_idx
 
 
-def process(task: hct.Task, sol_idx: int):
+# make the nn perform a task return the cost of the choice
+def nn_choice(task: hct.Task, sol_idx: int):
     global PREV_TIME, STATE
 
-    update_state_time(task)
+    p_idx = predict_with_cost(task.runners, task.p_costs, sol_idx)
+    a_idx = predict_with_cost(task.runners, task.a_costs, sol_idx)
+    prediction_cost = STATE[task.runners[p_idx]] + STATE[task.runners[a_idx]]
 
+    return prediction_cost
+
+
+# calculate the cost of the greedy choice
+def greedy_cost(task: hct.Task):
     min_cost = task.p_costs[0] + STATE[task.runners[0]] + task.a_costs[0] + STATE[task.runners[0]]
     for i in range(len(task.runners)):
         for j in range(len(task.runners)):
@@ -100,36 +111,34 @@ def process(task: hct.Task, sol_idx: int):
             if cost < min_cost:
                 min_cost = cost
 
-    p_idx = predict_with_cost(task.runners, task.p_costs, sol_idx)
-    a_idx = predict_with_cost(task.runners, task.a_costs, sol_idx)
-    prediction_cost = STATE[task.runners[p_idx]] + STATE[task.runners[a_idx]]
-
-    return min_cost, prediction_cost
+    return min_cost
 
 
-def fitness_func(solution, sol_idx):
-    # latency_sum = sum([process(task, sol_idx) for task in CURRENT_TASKS])
-    # latencies = [process(task, sol_idx) for task in CURRENT_TASKS]
-    min_costs = 0
-    prediction_costs = 0
+# percent difference from the greed choice
+def fitness_func(_, sol_idx):
+    c_greedy = 0
+    c_prediction = 0
     for task in CURRENT_TASKS:
-        min_cost, prediction_cost = process(task, sol_idx)
-        min_costs += min_cost
-        prediction_costs += prediction_cost
+        update_state_time(task)
+        c_greedy += greedy_cost(task)
+        c_prediction += nn_choice(task, sol_idx)
 
-    return (min_costs / (prediction_costs + 1)) * 100
+    return (c_greedy / (c_prediction + 1)) * 100
 
 
-def callback_generation(ga_instance):
+# At the end of each generation weights and tasks need to be updated
+def callback_generation(ga):
     global GANN_instance, CURRENT_TASKS, STATE, PREV_TIME
 
+    # update the weighs
     population_matrices = pygad.gann.population_as_matrices(population_networks=GANN_instance.population_networks,
-                                                            population_vectors=ga_instance.population)
+                                                            population_vectors=ga.population)
     GANN_instance.update_population_trained_weights(population_trained_weights=population_matrices)
 
-    print("Generation = {generation}".format(generation=ga_instance.generations_completed))
-    print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution()[1]))
+    print("Generation = {generation}".format(generation=ga.generations_completed))
+    print("Fitness    = {fitness}".format(fitness=ga.best_solution()[1]))
 
+    # update globals for the next generation
     STATE = numpy.zeros(RUNNER_COUNT)
     greedy(GREEDY_COUNT)
     print("state maximum: {n}".format(n=numpy.amax(STATE)))
@@ -137,6 +146,7 @@ def callback_generation(ga_instance):
     PREV_TIME = 0
 
 
+# how to we convert a task in json format to a Task object
 def converter(json_line):
     global RUNNER_COUNT
     task = hct.Task.from_json(json_line)
