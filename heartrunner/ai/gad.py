e8c270bda8a2bc66e0fa2ad87435c15e640c7740
BenjaminCB <benjamincb@hotmail.dk>
2022-11-08 10:07:44 +0100

    push before pc switch

--- heartrunner/ai/gad.py
+++ heartrunner/ai/gad.py
@@ -7,7 +7,7 @@ from random import uniform
 
 ALL_TASKS: list[hct.Task] = []
 CURRENT_TASKS: list[hct.Task] = []
-STATE = numpy.zeros(2500)
+STATE = numpy.zeros(20)
 PREV_TIME = 0
 SAMPLE_CHANCE = 0.03
 
@@ -38,9 +38,9 @@ def predict_with_cost(runners: list[int], cost: list[int], sol_idx: int):
 def process(task: hct.Task, sol_idx: int):
     global PREV_TIME, STATE
 
-    STATE -= (task.time - PREV_TIME)
-    STATE = STATE.clip(min=0)
-    PREV_TIME = task.time
+    # STATE -= (task.time - PREV_TIME)
+    # STATE = STATE.clip(min=0)
+    # PREV_TIME = task.time
 
     predict_with_cost(task.runners, task.p_costs, sol_idx)
     predict_with_cost(task.runners, task.a_costs, sol_idx)
@@ -49,8 +49,9 @@ def process(task: hct.Task, sol_idx: int):
 
 
 def fitness_func(solution, sol_idx):
-    latency_sum = sum([process(task, sol_idx) for task in CURRENT_TASKS])
-    return 10000000 / latency_sum
+    # latency_sum = sum([process(task, sol_idx) for task in CURRENT_TASKS])
+    latencies = [process(task, sol_idx) for task in CURRENT_TASKS]
+    return 10000000 / latencies[-1]
 
 
 def callback_generation(ga_instance):
@@ -70,13 +71,13 @@ def callback_generation(ga_instance):
 
 def converter(json_line):
     task = hct.Task.from_json(json_line)
-    task.runners = list(map(lambda r: r % 100, task.runners))
+    task.runners = list(map(lambda r: r % 20, task.runners))
     return task
 
 
 if __name__ == "__main__":
     with open("../../data/training/T3600-R2000-C10-A1.json", "r") as task_file:
-        ALL_TASKS = list(map(hct.Task.from_json, json.load(task_file)))
+        ALL_TASKS = list(map(converter, json.load(task_file)))
 
     # roughly 100 tasks
     CURRENT_TASKS = sample_tasks(SAMPLE_CHANCE)
@@ -84,26 +85,26 @@ if __name__ == "__main__":
     num_solutions = 6
     GANN_instance = pygad.gann.GANN(num_solutions=num_solutions,
                                     num_neurons_input=20,
-                                    num_neurons_hidden_layers=[40],
+                                    num_neurons_hidden_layers=[100,100,100,100,100],
                                     num_neurons_output=10,
                                     hidden_activations="relu",
                                     output_activation="softmax")
 
     population_vectors = pygad.gann.population_as_vectors(GANN_instance.population_networks)
 
-    ga_instance = pygad.GA(num_generations=500,
+    ga_instance = pygad.GA(num_generations=250,
                            num_parents_mating=4,
                            initial_population=population_vectors.copy(),
                            fitness_func=fitness_func,
-                           mutation_percent_genes=5,
-                           init_range_low=-2,
-                           init_range_high=2,
+                           mutation_percent_genes=10,
+                           init_range_low=-1,
+                           init_range_high=1,
                            parent_selection_type="tournament",
                            K_tournament=3,
                            crossover_type="single_point",
                            mutation_type="random",
-                           random_mutation_max_val=0.2,
-                           random_mutation_min_val=-0.2,
+                           random_mutation_max_val=0.05,
+                           random_mutation_min_val=-0.05,
                            keep_parents=0,
                            keep_elitism=0,
                            on_generation=callback_generation)
