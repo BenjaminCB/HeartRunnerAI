18326bd9c59e5f6dc4a2c8d2ed73d7e7b5dc356b
Pederbuus <peterbuussteffensen@gmail.com>
2022-11-21 13:07:24 +0100

    greedy_mip
    Helpe me

--- heartrunner/ai/gad.py
+++ heartrunner/ai/gad.py
@@ -48,6 +48,7 @@ def greedy(task_count: int):
     global STATE, PREV_TIME
 
     tasks = sample_n_tasks(task_count)
+    end_result = []
     for task in tasks:
         update_state_time(task)
         min_cost = task.p_costs[0] + STATE[task.runners[0]] + task.a_costs[1] + STATE[task.runners[1]]
@@ -73,8 +74,109 @@ def greedy(task_count: int):
 
         STATE[task.runners[p_idx]] += task.p_costs[p_idx]
         STATE[task.runners[a_idx]] += task.a_costs[a_idx]
+        end_result.append([a_idx, p_idx])
     PREV_TIME = 0
+    return end_result
 
+
+def greedy_mip(task_count: int):
+    global STATE, PREV_TIME
+    tasks = sample_n_tasks(2)
+
+    # The Assignment Problem, where 1 person can take 1 task, and then find the optimal match
+    # https://developers.google.com/optimization/assignment/assignment_example
+    # Worker1 [a.costs, p_costs, a_costs, p_costs ...] --- For each runner
+
+    num_tasks = len(tasks) * 2
+    # Total amount of runner IDs
+    num_workers = 2000
+    print(num_tasks)
+
+    # Use dummy value 10.000 for every entry we don't opdate, as 'none' 'null' and 'nothing' creates errors
+    costs = [[10000]*num_tasks for _ in range(num_workers)]
+    print(costs)
+    print(costs[0])
+
+    # Must use i, as we have 2 coulombs pr. task, but they stem from the same task
+    # Task iterates the coulombs in costs, as every task has 2 designated coulombs
+    # i iterates the tasks
+    i = 0
+    for task in range(0, num_tasks, 2):
+        # Iterate the amount of runners we consider for every task
+        for runner in range(10):
+            # Only update the costs for the runner IDs used in the task
+            runner_id = tasks[i].runners[runner]
+            costs[runner_id][task] = tasks[i].a_costs[runner] + STATE[tasks[i].runners[runner]]
+            costs[runner_id][task+1] = tasks[i].p_costs[runner] + STATE[tasks[i].runners[runner]]
+            print('a_cost', tasks[i].runners[runner], task)
+            print(costs[tasks[i].runners[runner]][task])
+            print('p_cost', tasks[i].runners[runner], task+1)
+            print(costs[tasks[i].runners[runner]][task+1])
+        i += 1
+
+    # Solver
+    # Create the mip solver with the SCIP backend.
+    solver = pywraplp.Solver.CreateSolver('SCIP')
+
+    if not solver:
+        return
+
+    # Variables
+    # x[i, j] is an array of 0-1 variables, which will be 1
+    # if worker i is assigned to task j.
+    x = {}
+    for i in range(num_workers):
+        for j in range(num_tasks):
+            x[i, j] = solver.IntVar(0, 1, '')
+
+    # Constraints
+    # Each worker is assigned to at most 1 task.
+    for i in range(num_workers):
+        solver.Add(solver.Sum([x[i, j] for j in range(num_tasks)]) <= 1)
+
+    # Each task is assigned to exactly one worker.
+    for j in range(num_tasks):
+        solver.Add(solver.Sum([x[i, j] for i in range(num_workers)]) == 1)
+
+    # Objective
+    objective_terms = []
+    for i in range(num_workers):
+        for j in range(num_tasks):
+            objective_terms.append(costs[i][j] * x[i, j])
+    solver.Minimize(solver.Sum(objective_terms))
+
+    # Solve
+    status = solver.Solve()
+    end_result = []
+
+    if status == pywraplp.Solver.OPTIMAL or status == pywraplp.Solver.FEASIBLE:
+        print(f'Total cost = {solver.Objective().Value()}\n')
+        # i is the 'actural' task, that is solved, is 1/2 of the current task iteration of the matrix 'costs'
+        i = 0
+        for task in range(0, num_tasks, 2):
+            for runner in range(num_workers):
+                # runner_id = tasks[i].runners[runner]
+                # Test if x[i,j] is 1 (with tolerance for floating point arithmetic).
+                if x[runner, task].solution_value() > 0.5:
+                    print('nr 1')
+                    STATE[tasks[i].runners[runner]] += tasks[i].a_costs[runner]
+                    # + STATE[tasks[i].runners[runner]]
+                    print(f'Worker {runner} assigned to task {task}.' +
+                          f' A_Cost: {costs[runner][task]}')
+                    end_result.append([runner, task])
+                if x[runner, (task+1)].solution_value() > 0.5:
+                    print('nr 2')
+                    STATE[tasks[i].runners[runner]] += tasks[i].p_costs[runner]
+                    # + STATE[tasks[i].runners[runner]]
+                    print(f'Worker {runner} assigned to task {task+1}.' +
+                          f' P_Cost: {costs[runner][task+1]}')
+                    end_result.append([runner, task+1])
+
+            i += 1
+    else:
+        print('No solution found.')
+
+    return end_result
 
 
 # make a prediction for a single choice such a which runner for the patient
@@ -161,7 +263,9 @@ if __name__ == "__main__":
     with open("../../data/training/T3600-R2000-C10-A1.json", "r") as task_file:
         ALL_TASKS = list(map(converter, json.load(task_file)))
 
-    greedy(GREEDY_COUNT)
+    print(greedy(GREEDY_COUNT))
+    print(greedy_mip(GREEDY_COUNT))
+    breakpoint()
     CURRENT_TASKS = sample_n_tasks(TASK_COUNT)
 
     GANN_instance = pygad.gann.GANN(num_solutions=10,
--- heartrunner/ai/gad_multi.py
+++ heartrunner/ai/gad_multi.py
@@ -80,27 +80,35 @@ def greedy(task_count: int):
         PREV_TIME = 0
 
 
-# fill the STATE by performing n amount of tasks using the greedy algorithm
+# Solve a MultiTask, my use of googles MIP
 def greedy_mip(task_count: int):
     global STATE, PREV_TIME
+    # TODO Insure we get the multitask correctly
     multitask = sample_n_tasks(task_count)
 
     # The Assignment Problem, where 1 person can take 1 task, and then find the optimal match
     # https://developers.google.com/optimization/assignment/assignment_example
-    # Worker1 [a.costs, a.costs, ... p_costs, p_costs ...] --- For each runner
+    # Worker1 [a.costs, p_costs, a_costs, p_costs ...] --- For each runner
 
     for mt in multitask:
+        # TODO How does update_state_time
         update_state_time(mt)
-        costs = []
         num_workers = len(mt.tasks[0].runners)
         num_tasks = len(mt.tasks) * 2
 
-        for task in range(0, task_count, 1):
+        # insert 0 in 'amount of col' for _ in 'amount of row'
+        costs = [[0] * num_tasks for _ in range(num_workers)]
+
+        i = 0
+        for task in range(0, num_tasks, 2):
             for runner in range(num_workers):
-                costs[task][runner] = mt.tasks[task].a_costs[runner] + STATE[mt.tasks[task].runners[runner]]
-        for task in range(task_count, task_count*2, 1):
-            for runner in range(num_workers):
-                costs[task][runner] = mt.tasks[task].p_costs[runner] + STATE[mt.tasks[task].runners[runner]]
+                costs[runner][task] = mt.tasks[i].a_costs[runner] + STATE[mt.tasks[i].runners[runner]]
+                costs[runner][task + 1] = mt.tasks[i].p_costs[runner] + STATE[mt.tasks[i].runners[runner]]
+                print(runner, task)
+                print(costs[runner][task])
+                print(runner, task + 1)
+                print(costs[runner][task + 1])
+            i += 1
 
         # Solver
         # Create the mip solver with the SCIP backend.
@@ -138,16 +146,20 @@ def greedy_mip(task_count: int):
 
         if status == pywraplp.Solver.OPTIMAL or status == pywraplp.Solver.FEASIBLE:
             print(f'Total cost = {solver.Objective().Value()}\n')
-            for i in range(num_workers):
-                for j in range(0, len(mt.tasks), 1):
+            # The 'actural' task, that is solved, is 1/2 of the curret task itteration of the matrix 'costs'
+            k = 0
+            for j in range(0, num_tasks, 2):
+                for i in range(num_workers):
                     # Test if x[i,j] is 1 (with tolerance for floating point arithmetic).
                     if x[i, j].solution_value() > 0.5:
-                        STATE[mt.tasks[j].runners[i]] += mt.tasks[j].a_costs[i] + STATE[mt.tasks[j].runners[i]]
-                        continue
-                for j in range(len(mt.tasks), len(mt.tasks) * 2, 1):
-                    if x[i, j].solution_value() > 0.5:
-                        STATE[mt.tasks[j].runners[i]] += mt.tasks[j].p_costs[i] + STATE[mt.tasks[j].runners[i]]
-                        continue
+                        STATE[mt.tasks[k].runners[i]] += mt.tasks[k].a_costs[i] + STATE[mt.tasks[k].runners[i]]
+                        print(f'Worker {i} assigned to task {j}.' +
+                              f' A_Cost: {costs[i][j]}')
+                    if x[i, j + 1].solution_value() > 0.5:
+                        STATE[mt.tasks[k].runners[i]] += mt.tasks[k].p_costs[i] + STATE[mt.tasks[k].runners[i]]
+                        print(f'Worker {i} assigned to task {j + 1}.' +
+                              f' P_Cost: {costs[i][j + 1]}')
+            k += 1
 
 
 # make a prediction for a single choice such a which runner for the patient
@@ -253,7 +265,8 @@ if __name__ == "__main__":
     with open("../../data/training/MT3600-R2000-C10-A1.json", "r") as task_file:
         ALL_TASKS = list(map(converter, json.load(task_file)))
 
-    greedy(GREEDY_COUNT)
+    #greedy(GREEDY_COUNT)
+    greedy_mip(GREEDY_COUNT)
     CURRENT_TASKS = sample_n_tasks(TASK_COUNT)
 
     GANN_instance = pygad.gann.GANN(num_solutions=10,
